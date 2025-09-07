dff0d4678e75b1fc920c0ab64c2f89f2eb489e8d
Dassel Bacaltos <dasselescalante013@gmail.com>
2025-09-07 09:08:48 +0000

    added 2d array methods

--- twoDarray.c
+++ twoDarray.c
@@ -1,7 +1,42 @@
 #include <stdio.h>
+#define SIZE 500
+
+int getInt() {
+    int n;
+    scanf("%d",&n);
+    return n;
+}
+
+void initialize(int twoDmatrix[][SIZE], int rows, int cols) {
+    for(int i=0; i<rows; i++) {
+        for(int j=0; j<cols; j++) {
+            twoDmatrix[i][j] = getInt();
+        }
+    }    
+}
+
+void display(int twoDmatrix[][SIZE], int rows, int cols) {
+    for(int i=0; i<rows; i++) {
+        for(int j=0; j<cols; j++) {
+            printf("%d ",twoDmatrix[i][j]);
+        }
+        printf("\n");
+    }    
+    
+}
 
 int main() {
-    int table[5][5];
+    int table[SIZE][SIZE];
+    int rows, cols;
+
+    printf("Enter the number of rows: ");
+    rows = getInt();
+
+    printf("Enter the number of columns: ");
+    cols = getInt();
+
+    initialize(table, rows, cols);
+    display(table, rows, cols);
 
 
     return 0;
