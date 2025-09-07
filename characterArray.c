298b163c8be1c4b45be2219e9bfcd549ceb9ab8f
Dassel Bacaltos <dasselescalante013@gmail.com>
2025-09-07 09:15:58 +0000

    added with string methods

--- characterArray.c
+++ characterArray.c
@@ -1,12 +1,34 @@
 #include <stdio.h>
+#include <string.h>
+#define MAX_SIZE 100
+
+int isPalindrome(char* s, int size) {
+    int left = 0, right = size - 1;
+    while(left < right) {
+        if(s[left] != s[right]) {
+            return 0; // false
+        }
+    }
+    return 1;
+}
 
 int main() {
     char str[] = {'J', 'o', 'h', 'n'}; // initialize by character
     char str2[] = "John"; // initialize via string literal
+    char str3[MAX_SIZE];
+    
     int i;
-    for(i=0; str[i]!='\0'; i++) { // iterate
+    for(i=0; str[i] != '\0'; i++) { // iterate
         printf("%c",str[i]);
     }
+    printf("\n");
+
+    printf("Enter a string: ");
+    fgets(str3, MAX_SIZE, stdin);
+
+    // remove null terminator
+    str3[strlen(str3) - 1] = '\0';
+    int size = strlen(str3);
 
     return 0;
 }
